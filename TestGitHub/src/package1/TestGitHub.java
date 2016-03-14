6335158d8694c861dac1aac794b33ff2f8e39663
enam97 <enam97@gmail.com>
2016-03-13 20:50:33 -0400

    dfd
    asdf

--- TestGitHub/src/package1/TestGitHub.java
+++ TestGitHub/src/package1/TestGitHub.java
@@ -4,7 +4,7 @@ public class TestGitHub {
 
 	public static void main(String[] args) {
 	
-	System.out.println("Today is 3/13/2016 Sunday. This is another line of text message.");
+	System.out.println("Today is 3/13/2016 Sunday. java -jar jenkins.war");
 
 	}
 
