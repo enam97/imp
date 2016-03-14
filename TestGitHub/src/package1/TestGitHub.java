6e10b6f33fe1e6ba4cc9b8544714bc1e75d799e8
enam97 <enam97@gmail.com>
2016-03-13 20:47:52 -0400

    3_13_2016a
    Description.a

--- TestGitHub/src/package1/TestGitHub.java
+++ TestGitHub/src/package1/TestGitHub.java
@@ -3,7 +3,7 @@ package package1;
 public class TestGitHub {
 
 	public static void main(String[] args) {
-	System.out.println("This test message is directly writen from eclipse and uploaded to Git Hub.");
+	
 	System.out.println("Today is 3/13/2016 Sunday. This is another line of text message.");
 
 	}
