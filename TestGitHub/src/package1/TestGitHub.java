e7a23e045fa73ae241ef5934f9753fd569853b0b
enam97 <enam97@gmail.com>
2016-03-13 20:45:21 -0400

    3_13_2016
    text changes

--- TestGitHub/src/package1/TestGitHub.java
+++ TestGitHub/src/package1/TestGitHub.java
@@ -4,7 +4,7 @@ public class TestGitHub {
 
 	public static void main(String[] args) {
 	System.out.println("This test message is directly writen from eclipse and uploaded to Git Hub.");
-	System.out.println("This is another line of text message.");
+	System.out.println("Today is 3/13/2016 Sunday. This is another line of text message.");
 
 	}
 
