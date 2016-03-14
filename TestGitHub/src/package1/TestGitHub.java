e9781c95bed2ac17b9abbf39e0ffc03da7ce9984
enam97 <enam97@gmail.com>
2016-03-13 21:02:20 -0400

    13Mar2016
    Des1

--- TestGitHub/src/package1/TestGitHub.java
+++ TestGitHub/src/package1/TestGitHub.java
@@ -4,7 +4,7 @@ public class TestGitHub {
 
 	public static void main(String[] args) {
 	
-	System.out.println("Today is 3/13/2016 Sunday. java -jar jenkins.war");
+	System.out.println("Today is 3/13/2016 Sunday. cd java -jar jenkins.war");
 
 	}
 
