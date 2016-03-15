703e5f145aa5228ec0114d50da2aa3f2135bbfc9
enam97 <enam97@gmail.com>
2016-03-14 22:55:31 -0400

    Pushing from Eclipse

--- TestGitHub/src/package1/TestGitHub.java
+++ TestGitHub/src/package1/TestGitHub.java
@@ -5,6 +5,7 @@ public class TestGitHub {
 	public static void main(String[] args) {
 	
 	System.out.println("Today is 3/14/2016 Sunday. Run Jenkins as 'cd java -jar jenkins.war' ");
+	System.out.println("Now I am committing and pushing this project from Eclipse");
 
 	}
 
