890b8d7d2b138d034c04fee6dab6aa66d47ad99e
enam97 <enam97@gmail.com>
2016-03-14 22:36:39 -0400

    updated

--- TestGitHub/src/package1/TestGitHub.java
+++ TestGitHub/src/package1/TestGitHub.java
@@ -4,7 +4,7 @@ public class TestGitHub {
 
 	public static void main(String[] args) {
 	
-	System.out.println("Today is 3/13/2016 Sunday. cd java -jar jenkins.war");
+	System.out.println("Today is 3/14/2016 Sunday. Run Jenkins as 'cd java -jar jenkins.war' ");
 
 	}
 
